134bcd276afabde2073916bda8b766ddbf705705
ambs <ambs@cpan.org>
2012-01-01 13:52:33 +0000

    New release 0.02_01

--- lib/Lingua/Identify/CLD.pm
+++ lib/Lingua/Identify/CLD.pm
@@ -11,11 +11,11 @@ Lingua::Identify::CLD - Interface to Chrome language detection library.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02_01
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02_01';
 
 use XSLoader;
 BEGIN {
